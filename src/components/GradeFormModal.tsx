070bbfffb4e33b39a0e66894eee92489b5d43964
Nathan <nathansec26@gmail.com>
2025-12-07 13:14:11 -0300

    style

--- src/components/GradeFormModal.tsx
+++ src/components/GradeFormModal.tsx
@@ -4,13 +4,11 @@ export default function GradeFormModal({ close }: { close: () => void }) {
   return (
     <div className="modal-overlay" onClick={close}>
       <div className="modal" onClick={(e) => e.stopPropagation()}>
-        <h2>Adicionar Disciplina</h2>
-
-        <GradeForm close={close} />
-
         <button className="close-btn" onClick={close}>
           Fechar
         </button>
+        <h2 className="ADD">Adicionar Disciplina</h2>
+        <GradeForm close={close} />
       </div>
     </div>
   );
